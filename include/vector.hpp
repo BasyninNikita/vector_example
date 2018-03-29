19856c011d9f3f808c325e2985531861aa0ab85e
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2018-03-29 22:25:35 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,148 +1,122 @@
 #include <iostream>
-#include <algorithm>
-#include <cassert>
-template <typename T>
-class vector_t
+#include <sstream>
+#include <string>
+
+using namespace std;
+class tree_t
 {
 private:
-	T * elements_;
-	std::size_t size_;
-	std::size_t capacity_;
+	struct node_t {
+		node_t * left=nullptr;
+		node_t * right;
+		int value=0;
+	};
+private:
+	node_t * root_;
 public:
-	vector_t();
-	vector_t(vector_t const & other);
-	vector_t & operator =(vector_t const & other);
-	~vector_t();
-	std::size_t size() const;
-	std::size_t capacity() const;
-	T & at(std::size_t index);
-	void push_back(T value);
-	void pop_back();
-	T & operator [](std::size_t index);
-	T operator [](std::size_t index) const;
-	bool operator ==(vector_t const & other) const;
-};
-template <typename T>
-vector_t<T>::vector_t()
-{
-	size_ = 0;
-	capacity_ = 0;
-	elements_ = new T [capacity_];
-}
-template <typename T>
-vector_t<T>::vector_t(vector_t<T> const & other)
-{
-	size_ = other.size_;
-	capacity_ = other.capacity_;
-	elements_ = new T [capacity_];	
-	for (std::size_t i=0; i<other.capacity_; i++) {
-		elements_[i] = other.elements_[i];
-	}	
-}
-template <typename T>
-T & vector_t<T>::at(std::size_t index)
-	{
-		if (index >= size_)
+	tree_t(){
+		root_=nullptr;
+	}
+void destr(node_t * node){
+ 		node_t * time=node;
+		while(time!=nullptr)
 		{
-			throw std::out_of_range("Error");
+			if(time->left!=nullptr)
+			{
+				time=time->left;
+			}
+			else if(time->right!=nullptr)
+			{
+				time=time->right;
+			}
+			else if((time->left==nullptr) && (time->right==nullptr) &&(time!=node))
+			{
+				delete time;
+				time=node;
+			}
+			else if((time->left==nullptr) && (time->right==nullptr) &&(time==node))
+			{
+				delete time;
+				break;
+			}
 		}
-		return (*this)[index];
+}
+~tree_t(){
+		destr(root);
 	}
-template <typename T>
-vector_t<T> & vector_t<T>::operator =(vector_t const & other)
-{
-	size_=other.size_;
-	capacity_=other.capacity_;
-	elements_ = new T[capacity_];
-	for(std::size_t i=0;i<other.capacity_;i++)
+	void insert(int value){
+		node_t * node=new node_t;
+		node->right=nullptr;
+		node->left=nullptr;
+		node->value=value;
+		if (root_==nullptr){
+			root_=node;
+			return;
+		}
+		node_t * vetka=root_;
+		while (branch!=nullptr){   
+			if(branch->value == value){
+			return;
+		}
+			else if(branch->value<value)
+			{
+				if(branch->right!=nullptr){
+				branch=branch->right;
+				}
+				else {
+					branch->right=node;
+					return;
+				}
+			}
+			else {
+				if(branch->left != nullptr){
+				branch=branch->left;
+			}
+			else{
+				branch->left=node;
+				return;
+			}
+		}
+		else return;
+	}
+}
+bool find(int value) const{
+	node_t * node=root;
+	while(node!=nullptr){
+		if (value=node->value)
+			return true;
+		else {
+			if (value<node->value){
+			node=node->left;	
+			}
+			else node=node->right;
+		}
+	}
+	return false;
+}
+void print(std::ostream & stream,int lvl,node_t* node){
+	if (node==nullptr)
+		return;
+		print(stream,lvl +1,node->right);
+			for (unsigned int i = 0; i < lvl; i++) {
+			stream << "---";
+		}
+		stream << node->value << endl;
+		print(stream, lvl + 1, node->left);
+	}
+void oper(std::ostream& stream, char op, int value){
+	if(op=='+')
 	{
-		elements_[i]=other.elements_[i];
+		insert(value);
 	}
-	return *this;
-}
-template <typename T>
-bool vector_t<T>::operator ==(vector_t const & other) const
-{
-	bool succ=false;
-	 if((size_==other.size_) && (capacity_==other.capacity_))
-	 {
-		 for(size_t i=0;i<other.size_;i++)
-		 {
-			 if(elements_[i]!=other.elements_[i])
-			 {
-				 return succ;
-			 }
-		 }
-		 succ=!succ;
-	 }
-	return succ;
-}
-template <typename T>
-vector_t<T>::~vector_t()
-{
-	delete []elements_;
-}
-template <typename T>
-std::size_t vector_t<T>::size() const
-{
-    return size_;
-}
-template <typename T>
-std::size_t vector_t<T>::capacity() const
-{
-    return capacity_;
-}
-template <typename T>
-void vector_t<T>::push_back(T value)
-{
-	if(capacity_==size_){
-		if(capacity_==0) 
-			capacity_=1;
-		else capacity_*=2;
-		T * p = new T [capacity_];
-        	for (std::size_t i=0; i<size_; i++) {
-            		p[i] = elements_[i];
-        	}
-        	delete [] elements_;
-        	elements_ = p;
-	}
-	elements_[size_++]=value;
-	
-}
-template <typename T>
-void vector_t<T>::pop_back()
-{
-    if(size_==0) return;
-    size_--;
-    if( size_ == capacity_/4 ){
-        capacity_ /=2;
-        T * p = new T [capacity_];
-        for (std::size_t i=0; i<size_; i++) {
-            p[i] = elements_[i];
-        }
-        delete [] elements_;
-        elements_ = p;
-    }
-}
-template <typename T>
-T & vector_t<T>::operator [](std::size_t index)
-{
-	return elements_[index];
-}
-template <typename T>
-T vector_t<T>::operator [](std::size_t index) const
-{
-	return elements_[index];
-}
-template <typename T>
-bool operator !=(vector_t<T> const & lhs, vector_t<T> const & rhs)
-{
-	bool succ=true;
-	if(lhs==rhs)
+	else if(op=='=')
 	{
-		succ=false;
+		print(value)
 	}
-	return succ;
+	else if(op=='?')
+	{
+		find(value);
+	}
+	else std::cout<<"incorrect operation";	
 }
-
-
+};
