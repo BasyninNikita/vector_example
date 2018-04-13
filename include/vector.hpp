f8cb78a0c924360cfd5c96ecfeb407701d44df64
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2018-04-13 13:25:09 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -168,7 +168,7 @@ void tree_t<T>:: oper2(char op, T value,std::ostream& stream){
 	else std::cout<<"incorrect operation";	
 	}
 template <typename T>
-bool tree<T>:: remove(T value){
+bool tree_t<T>:: remove(T value){
        if (root_ == nullptr)
        {
            return false;
